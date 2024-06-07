b7ca1b4a70f20ed73503b2d9144dbf353c961840
Elías <90994226+EliasRamil@users.noreply.github.com>
2024-06-07 11:46:24 -0300

    Proyecto de Spring

---
+++ grupo37/src/main/java/com/unla/grupo37/Grupo37Application.java
@@ -0,0 +1,13 @@
+package com.unla.grupo37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo37Application.class, args);
+	}
+
+}
---
+++ grupo37/src/test/java/com/unla/grupo37/Grupo37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
