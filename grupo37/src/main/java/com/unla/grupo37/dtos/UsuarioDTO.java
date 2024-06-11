b5bb326eeb5fbc12e635416415e15241493c675e
Torsico <167933591+Torsico@users.noreply.github.com>
2024-06-11 02:53:01 -0300

    remover imports que no se usan

--- grupo37/src/main/java/com/unla/grupo37/dtos/UsuarioDTO.java
+++ grupo37/src/main/java/com/unla/grupo37/dtos/UsuarioDTO.java
@@ -4,17 +4,6 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-
 public class UsuarioDTO {
 	private int id;
 
