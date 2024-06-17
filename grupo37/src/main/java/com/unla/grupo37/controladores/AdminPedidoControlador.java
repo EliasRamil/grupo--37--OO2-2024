9ca7ca431ac888bba33de84b5c46c9f64d17f283
Torsico <167933591+Torsico@users.noreply.github.com>
2024-06-16 22:05:12 -0300

    me olvide de subir esto xd

---
+++ grupo37/src/main/java/com/unla/grupo37/controladores/AdminPedidoControlador.java
@@ -0,0 +1,82 @@
+package com.unla.grupo37.controladores;
+
+import java.util.List;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.unla.grupo37.ayudante.AyudanteRutasVistas;
+import com.unla.grupo37.dtos.PedidoDTO;
+import com.unla.grupo37.dtos.ProductoDTO;
+import com.unla.grupo37.entidades.Pedido;
+import com.unla.grupo37.servicios.IPedidoServicio;
+import com.unla.grupo37.servicios.IProductoServicio;
+import com.unla.grupo37.servicios.IServicioGenerico;
+import com.unla.grupo37.servicios.implementacion.UsuarioRolServicio;
+import com.unla.grupo37.servicios.implementacion.UsuarioServicio;
+
+@Controller
+@RequestMapping("/admin/pedido")
+public class AdminPedidoControlador extends AbstractAdminVista {
+	
+	private IPedidoServicio pedidoServicio;
+	private IProductoServicio productoServicio;
+	private UsuarioRolServicio usuarioRolServicio;
+	private UsuarioServicio u;
+	
+	public AdminPedidoControlador(IPedidoServicio pedidoServicio, IProductoServicio productoServicio, UsuarioRolServicio usuarioRolServicio, UsuarioServicio u) {
+		this.pedidoServicio = pedidoServicio;
+		this.productoServicio = productoServicio;
+		this.usuarioRolServicio = usuarioRolServicio;
+		this.u = u;
+	}
+
+	@GetMapping("")
+    public ModelAndView pedido() {
+		String aux = permisoVista();
+		
+		if (!aux.equals("Ok")) return new ModelAndView(aux);
+		
+		ModelAndView mAV = new ModelAndView(AyudanteRutasVistas.ADMIN_PEDIDO);
+		List<ProductoDTO> listaProductos= productoServicio.findAllbyActivo();
+		mAV.addObject("productos", listaProductos);
+		
+        return mAV;
+    }
+	@PostMapping("{id}")
+	public String realizarPedido(@RequestParam String cantidad, @RequestParam String proveedor, @PathVariable(value="id") String id) throws Exception {
+		int idCliente = 0;
+
+	    // Obtener el nombre de usuario del usuario autenticado
+	    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal(); 
+	    if (principal instanceof UserDetails) {
+	        String username = ((UserDetails) principal).getUsername();
+	        
+	        idCliente = (u.traerUsuario(username)).getId();
+	    }
+	    
+	    Pedido pedido = new Pedido(proveedor, Integer.parseInt(cantidad), productoServicio.findByIdProducto(Integer.parseInt(id)),
+		usuarioRolServicio.getClienteById(idCliente), null);
+	    pedidoServicio.saveOne(pedido);
+		
+		return AyudanteRutasVistas.ADMIN_PEDIDO;
+	}
+	
+	@GetMapping("/lote")
+    public String lote() {
+		String aux = permisoVista();
+		
+		if(aux.equals("Ok"))
+			aux = AyudanteRutasVistas.ADMIN_LOTE;
+		
+        return aux;
+    }
+	
+}
---
+++ grupo37/src/main/java/com/unla/grupo37/servicios/IPedidoServicio.java
@@ -0,0 +1,8 @@
+package com.unla.grupo37.servicios;
+
+import com.unla.grupo37.dtos.PedidoDTO;
+import com.unla.grupo37.entidades.Pedido;
+
+public interface IPedidoServicio extends IServicioGenerico<Pedido> {
+
+}
