4e26c93631724a52befb5a6e812997c803d21451
Torsico <167933591+Torsico@users.noreply.github.com>
2024-06-17 20:06:23 -0300

    agregar IStockServicio.java (me olvide...)

---
+++ grupo37/src/main/java/com/unla/grupo37/servicios/IStockServicio.java
@@ -0,0 +1,13 @@
+package com.unla.grupo37.servicios;
+
+import com.unla.grupo37.dtos.StockDTO;
+import com.unla.grupo37.entidades.Compra;
+import com.unla.grupo37.entidades.Lote;
+import com.unla.grupo37.entidades.Stock;
+
+public interface IStockServicio extends IServicioGenerico<StockDTO> {
+	
+	boolean actualizarStock(Lote lot);
+	boolean actualizarStock(Compra com) throws Exception;
+	
+}
